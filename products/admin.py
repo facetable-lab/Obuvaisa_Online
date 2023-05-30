c1c68986e3902b51bc7280c15bddc7434aaddfc7
facetable-lab <diofff6@gmail.com>
2023-05-30 07:40:48 +0300

    Регистрация модели изображений для карусели

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import ProductCategory, Product
+from .models import ProductCategory, Product, CarouselImages
 
 admin.site.register(ProductCategory)
 admin.site.register(Product)
+admin.site.register(CarouselImages)
