aa978370c623d39a11d47926afeb129013748613
facetable-lab <diofff6@gmail.com>
2023-05-30 07:42:13 +0300

    Модель изображений карусели, русификация остальных моделей

--- products/models.py
+++ products/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class ProductCategory(models.Model):
-    name = models.CharField(max_length=64, unique=True)
-    description = models.TextField(blank=True)
+    name = models.CharField('Название', max_length=64, unique=True)
+    description = models.TextField('Описание', blank=True)
 
     def __str__(self):
         return self.name
@@ -14,13 +14,13 @@ class ProductCategory(models.Model):
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=256)
-    image = models.ImageField(upload_to='products_images', blank=True)
-    description = models.TextField(blank=True)
-    short_description = models.CharField(max_length=64, blank=True)
-    price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
-    quantity = models.PositiveIntegerField(default=0)
-    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    name = models.CharField('Название', max_length=256)
+    image = models.ImageField(upload_to='products_images', blank=True, verbose_name='Изображение')
+    description = models.TextField('Описание', blank=True)
+    short_description = models.CharField('Краткое описание', max_length=64, blank=True)
+    price = models.DecimalField('Цена', max_digits=8, decimal_places=2, default=0)
+    quantity = models.PositiveIntegerField('Остаток на складе', default=0)
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE, verbose_name='Категория')
 
     class Meta:
         verbose_name = 'Товар'
@@ -28,3 +28,14 @@ class Product(models.Model):
 
     def __str__(self):
         return f'{self.name} | {self.category}'
+
+
+class CarouselImages(models.Model):
+    image = models.ImageField(upload_to='carousel_images', verbose_name='Изображение')
+
+    def __str__(self):
+        return self.pk
+
+    class Meta:
+        verbose_name = 'Изображение'
+        verbose_name_plural = 'Изображения'
